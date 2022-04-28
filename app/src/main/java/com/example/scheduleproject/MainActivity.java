61bc84dee4415dc9e0dace46a46df4f5765552de
Noel <noeltsui0217@gmail.com>
2022-04-28 14:45:53 +0800

    Create Android Schedule Project

--- app/src/main/java/com/example/scheduleproject/MainActivity.java
+++ app/src/main/java/com/example/scheduleproject/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.scheduleproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView ScheduleListView;
+
+    Button btnAddSchedule = (Button) findViewById(R.id.btn_addSchedule);
+
+    public MainActivity(ListView scheduleListView) {
+        ScheduleListView = scheduleListView;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
